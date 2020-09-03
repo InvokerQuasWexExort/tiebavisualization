af17fbc00b2e69c0ab3efae8ced8450c93d6e24a
chenpeng <chen_peng_cp@126.com>
2020-09-03 15:55:50 +0800

    初始化

--- src/main/java/org/cp/tiebavisualization/crawler/TiebaCrawler.java
+++ src/main/java/org/cp/tiebavisualization/crawler/TiebaCrawler.java
@@ -1,6 +1,8 @@
 package org.cp.tiebavisualization.crawler;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.cp.tiebavisualization.pojo.Article;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -22,6 +24,8 @@ public class TiebaCrawler {
 
     private static final Logger LOG = LoggerFactory.getLogger(TiebaCrawler.class);
 
+    private static final String prefix = "https://tieba.baidu.com/";
+
     @Autowired
     private PhantomJsTemplate template;
 
@@ -40,12 +44,60 @@ public class TiebaCrawler {
             try {
                 Elements elements = document.select("div[id=pagelet_frs-list/pagelet/thread_list] ul#thread_list li.j_thread_list.clearfix");
                 if (elements != null && elements.size() > 0) {
-                    System.out.println(elements.size());
                     for (int i = 0; i < elements.size(); i++) {
+                        Article article = new Article();
                         Element element = elements.get(i);
                         String field = element.attr("data-field");
-                        System.out.println(field);
-                        System.out.println("----------------------->");
+                        JSONObject jsonObject = JSON.parseObject(field);
+                        if (jsonObject.containsKey("is_top")) {
+                            if (jsonObject.getBooleanValue("is_top")) {
+                                article.setIsTop(1);
+                            } else {
+                                article.setIsTop(0);
+                            }
+                        }
+                        if (jsonObject.containsKey("id")) {
+                            Long id = jsonObject.getLong("id");
+                            article.setTid(id);
+                        }
+                        if (jsonObject.containsKey("author_name")) {
+                            String authorName = jsonObject.getString("author_name");
+                            if (!StringUtils.isEmpty(authorName)) {
+                                article.setAuthorName(authorName);
+                            }
+                        }
+                        if (jsonObject.containsKey("author_nickname")) {
+                            String authorNickname = jsonObject.getString("author_nickname");
+                            if (!StringUtils.isEmpty(authorNickname)) {
+                                article.setAuthorNickname(authorNickname);
+                            }
+                        }
+                        if (jsonObject.containsKey("reply_num")) {
+                            Integer replyNum = jsonObject.getInteger("reply_num");
+                            article.setReplyNum(replyNum);
+                        }
+                        Elements userDiv = element.select("div.threadlist_title.pull_left.j_th_tit a");
+                        String title = userDiv.html();
+                        if (!StringUtils.isEmpty(title)) {
+                            article.setTitle(title);
+                        }
+                        String href = userDiv.attr("href");
+                        if (!StringUtils.isEmpty(href)) {
+                            article.setUrl(prefix + href);
+                        }
+                        Elements userSpan = element.select("span.tb_icon_author");
+                        String userDate = userSpan.attr("data-field");
+                        JSONObject user = JSON.parseObject(userDate);
+                        if (user.containsKey("user_id")) {
+                            Long userId = user.getLong("user_id");
+                            article.setUserId(userId);
+                        }
+                        Elements userUrlSpan = element.select("span.frs-author-name-wrap a");
+                        String userHref = userUrlSpan.attr("href");
+                        if (!StringUtils.isEmpty(userHref)) {
+                            article.setUserUrl(prefix + userHref);
+                        }
+                        System.out.println(article);
                     }
                 }
             } catch (Exception e) {
---
+++ src/main/java/org/cp/tiebavisualization/pojo/Article.java
@@ -0,0 +1,129 @@
+package org.cp.tiebavisualization.pojo;
+
+import java.util.Date;
+
+/**
+ * @ClassName Article
+ * @Description
+ * @Author chenpeng
+ * @Date 2020/9/2 19:30
+ */
+public class Article {
+
+    private int id;
+    private long tid;
+    private long userId;
+    private String authorName;
+    private String authorNickname;
+    private String title;
+    private String url;
+    private String userUrl;
+    private int replyNum;
+    private Date createTime;
+    private int isTop;
+
+    public int getIsTop() {
+        return isTop;
+    }
+
+    public void setIsTop(int isTop) {
+        this.isTop = isTop;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getTid() {
+        return tid;
+    }
+
+    public void setTid(long tid) {
+        this.tid = tid;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorNickname() {
+        return authorNickname;
+    }
+
+    public void setAuthorNickname(String authorNickname) {
+        this.authorNickname = authorNickname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReplyNum() {
+        return replyNum;
+    }
+
+    public void setReplyNum(int replyNum) {
+        this.replyNum = replyNum;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUserUrl() {
+        return userUrl;
+    }
+
+    public void setUserUrl(String userUrl) {
+        this.userUrl = userUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", tid=" + tid +
+                ", userId=" + userId +
+                ", authorName='" + authorName + '\'' +
+                ", authorNickname='" + authorNickname + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", userUrl='" + userUrl + '\'' +
+                ", replyNum=" + replyNum +
+                ", createTime=" + createTime +
+                ", isTop=" + isTop +
+                '}';
+    }
+}
